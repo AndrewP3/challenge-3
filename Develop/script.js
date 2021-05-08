2e51361d79733f4baa4245ff69ee5f296266daeb
AndrewP3 <andrewpowell097@gmail.com>
2021-05-08 14:55:44 -0700

    Second commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
-// Assignment code here
+// Generator functions
+var plength = prompt("How many characters does your password need? Please pick a number between 8 and 128.");
+
+while (plength <8 || plength >128) {
+
+  if (plength >=8 && plength <=128) {
+    alert("You chose" + plength)
+  }
+  else {prompt("Please choose a password length between 8 and 128.")
+  }
+};
+
+var userInput = []
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var number = [0,1,2,3,4,5,6,7,8,9];
+
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
 
 
 // Get references to the #generate element
@@ -13,5 +34,8 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// This is to generate password button functionality 
+document.getElementbyId("generate").addEventListener("click", function(){
+  var password = generate (plength)
+  document.getElementById("password").value = password;
+});
